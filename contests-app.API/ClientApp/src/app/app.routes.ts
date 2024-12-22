25a0f5ad5b797a2871adcc7df7ddeda560ebebb9
Aleksey Batuev <n.batuev@infinnity.ru>
2024-12-22 19:19:00 +0500

    removed extras

--- contests-app.API/ClientApp/src/app/app.routes.ts
+++ contests-app.API/ClientApp/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'prefix',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      }
+    ]
+  }
+];
---
+++ contests-app.API/ClientApp/src/app/core/auth/pages/login/login.component.ts
@@ -0,0 +1,41 @@
+import { NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatButton,
+    NgIf
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent {
+  @Input() error: string | null = null;
+
+  @Output() submitEM = new EventEmitter();
+
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+    }
+  }
+}
