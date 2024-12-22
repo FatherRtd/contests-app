db9b3c2a0c068cfed1dc190e3bb0abaeb66feae5
Aleksey Batuev <n.batuev@infinnity.ru>
2024-12-22 17:57:16 +0500

    onpush

--- contests-app.API/ClientApp/src/main.ts
+++ contests-app.API/ClientApp/src/main.ts
@@ -1,16 +1,6 @@
-import './assets/main.css'
-import 'element-plus/dist/index.css'
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-import ElementPlus from 'element-plus'
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
-
-import App from './App.vue'
-import router from './router'
-const app = createApp(App)
-
-app.use(ElementPlus)
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
