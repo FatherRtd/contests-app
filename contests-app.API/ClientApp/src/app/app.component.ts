ee8327ca6b6ff325f8a41d0acaa7d4fac9a2d0f7
Aleksey Batuev <n.batuev@infinnity.ru>
2024-12-22 21:45:09 +0500

    a

--- contests-app.API/ClientApp/src/app/app.component.ts
+++ contests-app.API/ClientApp/src/app/app.component.ts
@@ -2,8 +2,6 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { AuthService } from './core/auth/services/auth.service';
-
 @Component({
     selector: 'app-root',
     standalone: true,
@@ -12,16 +10,4 @@ import { AuthService } from './core/auth/services/auth.service';
     styleUrl: './app.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-    constructor(private readonly auth: AuthService) {
-        this.auth.isAuthenticated$.subscribe();
-        this.auth.isAuthenticated$.subscribe();
-        this.auth.isAuthenticated$.subscribe();
-        this.auth.isAuthenticated$.subscribe();
-        this.auth.user$.subscribe();
-        this.auth.user$.subscribe();
-        this.auth.user$.subscribe();
-        this.auth.user$.subscribe();
-        this.auth.user$.subscribe();
-    }
-}
+export class AppComponent {}
